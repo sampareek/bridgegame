e12b569e02fd34aaf7fc5cd0c95c4a0a1fabe43a
ujjavalmoradiya <ujjavalmoradiya@gmail.com>
2015-06-01 19:52:18 +0530

    Scripts added by Bonny

---
+++ Scripts/Hands.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hands : MonoBehaviour {
+	private int[] handCards;
+	private char firstTurn;
+	private char handWinner;
+
+	public Hands(char player)
+	{
+		firstTurn = player;
+		handCards = new int[4];
+	}
+
+	public void findHighCard(string trump_suit)
+	{
+		//build logic and decides handWinner
+	}
+
+	public void addCard()
+	{
+		// build logic... this adds cards to handcards;
+	}
+
+	public void setFirstTurn(char player)
+	{
+		firstTurn = player;
+	}
+
+	public char gerFirstTurn()
+	{
+		return firstTurn;
+	}
+
+	public char gethandWinner()
+	{
+		return handWinner;
+	}
+
+}
---
+++ Scripts/Table.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Table : MonoBehaviour {
+
+
+	public Table()
+	{
+
+	}
+
+	public void playCard()
+	{
+		//user plays a card
+	}
+
+	public bool validateCard()
+	{
+		//checks card is valid or not 
+	}
+
+	public void countHands()
+	{
+		//logic part
+		//difine ogject for hands 
+	}
+
+	public void setBidStarter()
+	{
+		//logic part
+	}
+
+	public void setDummy()
+	{
+		///logic part;
+	}
+
+	public void fetchCardsforDuplicate()
+	{
+		// logic part;
+	}
+
+	private int tableid;
+	private Player northPlayer;
+	private Player southPlayer;
+	private Player westPlayer;
+	private Player eastPlayer;
+	private string previousBid;
+	private Hands[] allHands;
+	private int passcounter;
+	private string maxBid;
+	private string trumpSuit; // heart / spade / diamond / club
+	private int points;
+	private int hands_NS; // stores hands acquired by north and south
+//	private int hands_EW; can fetch by 13-hands_NS
+}
