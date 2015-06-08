bd1851184e20b889a8cd13197ee5f32778ac956c
ujjavalmoradiya <ujjavalmoradiya@gmail.com>
2015-06-08 20:31:53 +0530

    updated By Bonny

---
+++ BridgeGame/Assets/Scripts/LoadCards.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadCards : MonoBehaviour {
+	
+	private GameObject[] cards;
+	private GameObject prefab_card;
+
+	void Start () 
+	{
+		cards = GameObject.FindGameObjectsWithTag ("Cards_backimage");
+
+		//while(cards[0])
+			StartCoroutine (MoveTowardsTarget(0));
+
+			StartCoroutine (MoveTowardsTarget(1));
+	}
+
+	void Update()
+	{
+
+	}
+
+	private void placeCards()
+	{
+
+	}
+
+	Vector3 getTargetPosition(int index)
+	{
+			float x = -4.50f;
+			Vector3 position = new Vector3 ();
+			position.x = x + index;
+			position.y = -3.50f;
+			position.z = 0;
+
+		return position;
+	}
+
+	Vector3 getCurrentPosition(GameObject card)
+	{
+		return card.transform.position;
+	}
+
+	protected IEnumerator MoveTowardsTarget(int i) 
+	{
+		while (Vector3.Distance (getCurrentPosition (cards [i]), getTargetPosition (i)) > .1f) {
+			cards [i].transform.position = Vector3.Lerp (getCurrentPosition (cards [i]), getTargetPosition (i), 2f * Time.deltaTime);
+			yield return null;
+		}
+		Destroy(cards[i]);
+		prefab_card = (GameObject)Resources.Load ("Prefabs/cards_c2");
+		
+		Vector3 card_position = new Vector3 ();
+		card_position = getCurrentPosition (cards [i]);
+		Instantiate (prefab_card, card_position, Quaternion.identity);
+	}
+}
---
+++ BridgeGame/Assets/Scripts/SampleMobe.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleMobe : MonoBehaviour {
+
+	public float moveTime = 0.1f;           //Time it will take object to move, in seconds.
+	
+	private Rigidbody2D rb2D;               //The Rigidbody2D component attached to this object.
+	private float inverseMoveTime;          //Used to make movement more efficient.
+	public GameObject[] cards;
+	int c = 0;
+	protected virtual void Start ()
+	{
+	//	Joint.FindObjectsOfType(Rigidbody2D) =  GameObject.FindGameObjectsWithTag("Cards_backimage");
+	//	rb2D = GetComponents <Rigidbody2D> ();
+		cards = GameObject.FindGameObjectsWithTag ("Cards_backimage");
+	//	Debug.Log (cards[0].name);
+	//	Debug.Log (cards[1].name);
+		rb2D = cards [0].GetComponent<Rigidbody2D> ();
+
+	//	Debug.Log (rb2D.name);
+		inverseMoveTime = 1f / moveTime;
+		Vector3 end = new Vector3 ();
+			end.x = -6f;
+			end.y = -4f;
+			end.z = 0f;
+		StartCoroutine(SmoothMovement (end));
+
+		rb2D = cards [1].GetComponent<Rigidbody2D> ();
+		StartCoroutine(SmoothMovement (end));
+	}
+
+	protected IEnumerator SmoothMovement (Vector3 end)
+	{
+		//Calculate the remaining distance to move based on the square magnitude of the difference between current position and end parameter. 
+		//Square magnitude is used instead of magnitude because it's computationally cheaper.
+		float sqrRemainingDistance = (rb2D.transform.position - end).sqrMagnitude;
+		
+		//While that distance is greater than a very small amount (Epsilon, almost zero):
+		while(sqrRemainingDistance > float.Epsilon)
+		{
+			//Find a new position proportionally closer to the end, based on the moveTime
+			Vector3 newPostion = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+			
+			//Call MovePosition on attached Rigidbody2D and move it to the calculated position.
+			rb2D.MovePosition (newPostion);
+			
+			//Recalculate the remaining distance after moving.
+			sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+			
+			//Return and loop until sqrRemainingDistance is close enough to zero to end the function
+			yield return null;
+		}
+	}		
+}
